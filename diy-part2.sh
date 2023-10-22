57b3bcf475245669bb57fdc7d4fc9829a06e2a83
2279244533 <131430121+2279244533@users.noreply.github.com>
2023-10-22 10:46:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,9 +6,17 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 添加源
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 应用过滤OpenAppFilter 【luci oaf app】
+# 和加速模块（Turbo ACC)有冲突，需要关闭Turboo ACC后使用
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
